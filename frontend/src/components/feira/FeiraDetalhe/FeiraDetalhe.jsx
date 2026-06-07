b6b95db247690339272db789c11cc35917ef9d14
arthur-rodrigues <arthurodrigues4433@gmail.com>
2026-06-07 12:45:32 -0300

    ajustei a rota do scan que não estava funcional

--- frontend/src/components/feira/FeiraDetalhe/FeiraDetalhe.jsx
+++ frontend/src/components/feira/FeiraDetalhe/FeiraDetalhe.jsx
@@ -36,7 +36,7 @@ const FeiraDetalhe = () => {
     };
 
     const handleScanProduct = () => {
-        navigate(`/scan/${id}`);
+        navigate(`/feira/${id}/scan`);
     };
 
     const handleFinalizar = async () => {
